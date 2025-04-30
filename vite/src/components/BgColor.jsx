a1ba788d0bbeac22fb5794f7c07d1ffac230b2c0
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-04-30 22:03:26 +0500

    react.js practice

--- vite/src/components/BgColor.jsx
+++ vite/src/components/BgColor.jsx
@@ -4,9 +4,9 @@ const BgColor = () => {
     const [color, setColor] = useState("black")
     return (
         <div className="w-full h-screen" style={{ background: color }}>
-            <button className='p-3 m-1 rounded-xl' onClick={() => setColor("red")} style={{ background: "red" }}>Red</button>
-            <button className='p-3 m-1 rounded-xl' onClick={() => setColor("blue")} style={{ background: "blue" }}>Blue</button>
-            <button className='p-3 m-1 rounded-xl' onClick={() => setColor("green")} style={{ background: "green" }}>Green</button>
+            <button className='p-4 m-2 rounded-xl' onClick={() => setColor("red")} style={{ background: "red" }}>Red</button>
+            <button className='p-4 m-2 rounded-xl' onClick={() => setColor("blue")} style={{ background: "blue" }}>Blue</button>
+            <button className='p-4 m-2 rounded-xl' onClick={() => setColor("green")} style={{ background: "green" }}>Green</button>
         </div>
     )
 }
