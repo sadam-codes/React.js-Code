a3a43b93f0b24e21731f039d85e000956ea38662
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-05-03 17:25:17 +0500

    installing react-router-dom and making header component

--- 03-router/src/App.jsx
+++ 03-router/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Header from './components/Header'
 function App() {
   return (
     <>
-      <h1 className="p-3 bg-black text-white">Hello</h1>
+     <Header/>
     </>
   )
 }
---
+++ 03-router/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='container mx-auto p-3'>
+            <h1 className='bg-black text-white rounded-2xl p-4'>Hello Header</h1>
+        </div>
+    )
+}
+export default Header
