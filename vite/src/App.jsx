bdda3a9e2941fed97a1dc7000aca7ec4c5172e09
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-05-03 18:47:13 +0500

    react-routers all setup and all component and active using state

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -4,15 +4,30 @@ import Props2 from './components/Props2'
 import BgColor from './components/BgColor'
 import GithubApi from './components/GithubApi'
 import GithubAxios from './components/GithubAxios'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from './RoutingComponents/Home'
+import About from './RoutingComponents/About'
+import Contact from './RoutingComponents/Contact'
+import Projects from './RoutingComponents/Projects'
+import Header from './RoutingComponents/Header'
 
 const App = () => {
   return (
     <>
-      <Props2/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/project' element={<Projects />} />
+        </Routes>
+      </Router>
+      <Props2 />
       <Counter />
-      <GithubApi/>
-      <GithubAxios/>
-      <BgColor/>
+      <GithubApi />
+      <GithubAxios />
+      <BgColor />
     </>
   )
 }
---
+++ vite/src/RoutingComponents/Header.jsx
@@ -0,0 +1,18 @@
+import React, { act, useState } from 'react'
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    const [active, setActive] = useState("/")
+    return (
+        <div className='bg-black text-white p-5 mx-auto container rounded-2xl'>
+            <div className='rounded-2xl flex justify-between align-center'>
+                <Link to="/" onClick={() => setActive("/")} className={active === "/" ? "text-red-500" : "text-white"}>Home</Link>
+                <Link to="/about" onClick={() => setActive("/about")} className={active === "/about" ? "text-red-500" : "text-white"}>About</Link>
+                <Link to="/contact" onClick={() => setActive("/contact")} className={active === "/contact" ? "text-red-500" : "text-white"}>Contact</Link>
+                <Link to="/project" onClick={() => setActive("/project")} className={active === "/project" ? "text-red-500" : "text-white"}>Project</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
