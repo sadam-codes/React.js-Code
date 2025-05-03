61256c44cd736d376858affcdc8d02e60d105701
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-05-03 18:03:50 +0500

    react-routers all setup and all component

--- 03-router/src/App.jsx
+++ 03-router/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 function App() {
   return (
     <>
-     <Header/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
   )
 }
-
 export default App
--- 03-router/src/components/Header.jsx
+++ 03-router/src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link, useLocation } from "react-router-dom"
 
 const Header = () => {
+    const location = useLocation();
     return (
-        <div className='container mx-auto p-3'>
-            <h1 className='bg-black text-white rounded-2xl p-4'>Hello Header</h1>
+        <div className='container mx-auto p-3 bg-black text-white rounded-2xlflex justify-between align-center'>
+            <div className='flex justify-between p-5'>
+                <Link to="/" className={location.pathname === "/" ? "text-red-500" : "text-white"}>Home</Link>
+                <Link to="/about" className={location.pathname === "/about" ? "text-red-500" : "text-white"}>About</Link>
+                <Link to="/contact" className={location.pathname === "/contact" ? "text-red-500" : "text-white"}>Contact</Link>
+            </div>
         </div>
     )
 }
