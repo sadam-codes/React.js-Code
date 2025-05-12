613f78d6b524543b264eb5a0d82e2523f92550c9
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-05-12 23:47:56 +0500

    updated

--- 03-router/src/components/About.jsx
+++ 03-router/src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div className='container mx-auto'>
-      <h1>About Page</h1>
+      <h1>This is About Page</h1>
     </div>
   )
 }
--- 03-router/src/components/Contact.jsx
+++ 03-router/src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
     return (
         <div className='container mx-auto'>
-            <h1>Contact page</h1>
+            <h1>This is Contact page</h1>
         </div>
     )
 }
